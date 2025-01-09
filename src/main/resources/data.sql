156daf64b24aef8c96bdfda5dab355d98bb444e2
josmyrose-03 <157198814+josmyrose-03@users.noreply.github.com>
2025-01-09 15:33:12 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Movie_details (movie_title, movie_language, movie_release_date, movie_image_url)
+VALUES
+('Inception', 'English', '2010-07-16', ''),
+('Baahubali', 'Telugu', '2015-07-10', ''),
+('Parasite', 'English', '2019-05-30', ''),
+('Dangal', 'Hindi', '2016-12-21', '');
